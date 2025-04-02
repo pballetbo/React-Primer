824443e994d6fc8696fe5f4a7f44c9e88143e226
pballetbo <polballetbo.13@gmail.com>
2025-04-02 19:57:47 +0200

    Updates

---
+++ precache-manifest.b001e6bfb72464c2996e4dd20cc6a6f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b65739e1ee6b98f9f11daaa9231a700",
+    "url": "/React-Primer/index.html"
+  },
+  {
+    "revision": "1f461ac4c10a6dc00b10",
+    "url": "/React-Primer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/React-Primer/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/React-Primer/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "1f461ac4c10a6dc00b10",
+    "url": "/React-Primer/static/js/main.8ade8694.chunk.js"
+  },
+  {
+    "revision": "21c1ac17fa8901be5dcb",
+    "url": "/React-Primer/static/js/runtime~main.89aee531.js"
+  }
+]);
